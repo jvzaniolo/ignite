b87090a288586356e82ddcf38c7550ec89c198dc
João Vitor <jv.zaniolo@gmail.com>
2021-04-08 18:20:59 -0300

    Finish chapter 2 - Componentização - Header

--- react/02-dtmoney/src/App.tsx
+++ react/02-dtmoney/src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header'
 import GlobalStyles from './styles/global'
 
 export function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <>
+      <Header />
       <GlobalStyles />
-    </div>
+    </>
   )
 }
---
+++ react/02-dtmoney/src/components/Header/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  background: var(--purple);
+`
+
+export const Content = styled.div`
+  margin: 0 auto;
+  max-width: 1120px;
+
+  display: flex;
+  align-items: center;
+  padding: 2rem 1rem 10rem;
+  justify-content: space-between;
+
+  button {
+    border: 0;
+    color: white;
+    font-size: 1rem;
+    padding: 0.5rem 2rem;
+    border-radius: 0.25rem;
+    background: var(--purple-light);
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`
