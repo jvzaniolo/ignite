0a28797fb4bd36c527aa4a7b2344764d7e0710b1
João Vitor <jv.zaniolo@gmail.com>
2021-04-23 18:00:05 -0300

    Add subscriptions to faunaDB

--- react/03-ig-news/src/components/SubscribeButton.tsx
+++ react/03-ig-news/src/components/SubscribeButton.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/client';
+import { getStripeJS } from '../services/stripe-js';
+import { api } from '../services/api';
+
 import styles from '../styles/SubscribeButton.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +9,26 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId: response.data.sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.button}>
+    <button type="button" className={styles.button} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
