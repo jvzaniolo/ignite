ab35c7eaa1945379cd1cf2aae5f19cd6d0d2bd58
João Vitor <jv.zaniolo@gmail.com>
2024-06-08 12:27:37 -0300

    add auth controller

---
+++ nodejs/03-api-solid/src/http/controllers/authenticate.ts
@@ -0,0 +1,29 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { PrismaUserRepository } from "~/repositories/prisma/PrismaUserRepository";
+import { AuthenticateService } from "~/services/Authenticate";
+import { InvalidCredentialsError } from "~/services/_errors";
+
+export async function authenticate(req: FastifyRequest, res: FastifyReply) {
+  let authenticateSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
+  let { email, password } = authenticateSchema.parse(req.body);
+
+  try {
+    const userRepository = new PrismaUserRepository();
+    const authenticateService = new AuthenticateService(userRepository);
+
+    await authenticateService.execute({ email, password });
+
+    return res.status(200).send();
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return res.status(409).send({ message: error.message });
+    }
+
+    throw error;
+  }
+}
